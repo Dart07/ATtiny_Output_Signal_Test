e2e84423c39395bbe262bc424d0153843e3de9aa
Dart07 <danalind@gmail.com>
2015-04-20 01:28:51 -0500

    bitmasking works
    I used this website http://www.arduino.cc/en/Tutorial/BitMask . It
    shifts the bit to the left with teh left shift operator.

--- ATtiny_Output_Signal_Test.ino
+++ ATtiny_Output_Signal_Test.ino
@@ -2,6 +2,8 @@
 #define ATtiny_PIN3   4     //ATtiny Pin 3
 #define ATtiny_PIN2   3     //ATtiny Pin 2
 byte data[2] = {};
+byte mask = 1;
+byte startByte = 0x55;
 void setup() {
 // no need to set up, just run through the loop
 data[0] = 0xFF;
@@ -21,6 +23,17 @@ void loop() {
   sendData(data[0]);
   sendEnd();
   */
+}
+void sendStart(){
+   for (mask = 00000001; mask>0; mask <<= 1) { //iterate through bit mask
+    if (startByte & mask){ // if bitwise AND resolves to true
+      digitalWrite(ATtiny_PIN2,HIGH); // send 1
+    }
+    else{ //if bitwise and resolves to false
+      digitalWrite(ATtiny_PIN2,LOW); // send 0
+    }
+    delay(1000); //delay
+  } 
 }
 // not going to work since it is a btye and not separate bits in an array...
 /*void sendStart(){
@@ -46,7 +59,7 @@ void loop() {
   }
 }
 */
-void sendStart(){
+/*void sendStart(){
   byte startByte = B10101010;
   for(int x = 0; x < 7; x++){
     if(startByte & B00000001 == B00000001){
@@ -59,7 +72,7 @@ void sendStart(){
     }
     startByte >> 1;    
   }
-}
+}*/
 /*
 void sendData(byte data){
   for(int i = 0; i < sizeof(data)-1; i++){
